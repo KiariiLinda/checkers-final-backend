c2489cf4ffd6fd9cb2eba8fdaede2a5fc33949c7
Marc Ndegwa <macndegwa11296@gmail.com>
2024-07-31 17:20:37 +0300

    small change

--- app/__init__.py
+++ app/__init__.py
@@ -3,16 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from .config import Config
 
-from flask_cors import CORS
+
 
 db = SQLAlchemy()
 migrate = Migrate()
+
 
 def create_app():
     app=Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
     migrate.init_app(app, db)
-
-    CORS(app)
+   
     return app
