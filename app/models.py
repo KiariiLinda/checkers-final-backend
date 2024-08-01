ef5c21170cfb322a194af496a7a208a9a89794a4
Jalenzz <126164913+Jalenzzz@users.noreply.github.com>
2024-08-01 15:29:45 +0300

    Small change

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class Users(db.Model):
     username = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
-    games = db.relationship('Game',backref='Users',uselist=True,cascade='all, delete-orphan')
+    games = db.relationship('Games',backref='users',uselist=True,cascade='all, delete-orphan')
 
     def details(self):
         return({'id':self.id,'username':self.username,'email':self.email})
@@ -15,7 +15,7 @@ class Users(db.Model):
 class Games(db.Model):
     __tablename__ = 'games'
     id = db.Column(db.BigInteger, primary_key=True)
-    player1_id = db.Column(db.BigInteger,db.ForeignKey('users.id',ondelete='CASCADE'),nullable=False)
+    player_id = db.Column(db.BigInteger,db.ForeignKey('users.id',ondelete='CASCADE'),nullable=False)
     board_state = db.Column(db.Text, nullable=False)
     human_won = db.Column(db.Boolean, nullable=True)
 
