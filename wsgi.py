f7c5e9e1d41b680aa1ffe0adcf2f148ad66215c1
Jalenzz <126164913+Jalenzzz@users.noreply.github.com>
2024-08-15 14:46:38 +0300

    Deploying to render

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,10 @@
 from app import create_app
 
 app = create_app()
+
 @app.route('/')
 def home():
     return 'Welcome to our checkers game backend!'
 
 if __name__ == '__main__':
-   app.run(debug=True,port=9000)
+   app.run(debug=True, port=9000)
